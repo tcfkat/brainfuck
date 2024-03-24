adc208493cc825e25f369af0f968356132e121cb
tcfkat <30535842+tcfkat@users.noreply.github.com>
2024-03-24 06:54:25 +0100

    Update brainfuck.c

--- brainfuck.c
+++ brainfuck.c
@@ -1,4 +1,4 @@
-// Short implementation of programming language Brainfuck. tcfkat 20240321
+// Short implementation of programming language Brainfuck. tcfkat 20240324
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -6,35 +6,35 @@
 #define IS 10000//Input size
 static uint8_t *t;//Tape
 static int16_t tp;//Tape pointer
-static char *in,*inp;//Input buffer
+static char *i,*ip;//Input buffer and pointer
 static void bs(char a,char b,uint8_t dir)//bracket search
 {
-uint8_t bc=0;
+uint8_t bc=0;//bracket counter
     while(1){
-        if(*inp==a)bc++;
-        if(*inp==b){bc--;if(!bc)return;}
-        dir?inp++:inp--;
-        if(inp<in||*inp==0){printf("No matching %c\n",b);free(t);free(in);exit(1);}
+        if(*ip==a)bc++;
+        if(*ip==b){bc--;if(!bc)return;}
+        dir?ip++:ip--;
+        if(ip<i||*ip==0){printf("No matching %c\n",b);free(t);free(i);exit(1);}
     }
 }
 int main(void)
 {
     t=calloc(TS,1);
-    inp=in=calloc(IS,1);
-    if(!t||!in){if(t)free(t);if(in)free(in);printf("No mem\n");return 1;}
-    fgets(in,IS-2,stdin);//Minor bug: \n terminates input, no multi-line input
+    ip=i=calloc(IS,1);
+    if(!t||!i){if(t)free(t);if(i)free(i);printf("No mem\n");return 1;}
+    fgets(i,IS-2,stdin);//Minor bug: \n terminates input, no multi-line input
     while(1){
-        switch(*inp){
-            case'>':if(++tp>=TS)tp=0;inp++;break;//Wrap around end of tape
-            case'<':if(--tp<0)tp=TS-1;inp++;break;//Wrap around begin of tape
-            case'+':++t[tp];inp++;break;
-            case'-':--t[tp];inp++;break;
-            case'.':putchar(t[tp]);inp++;break;
-            case',':t[tp]=(uint8_t)*inp;inp++;break;
-            case'[':if(t[tp]){inp++;}else{bs('[',']',1);inp++;};break;
+        switch(*ip){
+            case'>':if(++tp>=TS)tp=0;ip++;break;//Wrap around end of tape
+            case'<':if(--tp<0)tp=TS-1;ip++;break;//Wrap around begin of tape
+            case'+':++t[tp];ip++;break;
+            case'-':--t[tp];ip++;break;
+            case'.':putchar(t[tp]);ip++;break;
+            case',':t[tp]=(uint8_t)*ip;ip++;break;
+            case'[':if(!t[tp])bs('[',']',1);ip++;break;
             case']':bs(']','[',0);break;
-            case 0:free(t);free(in);return 0;
-            default:inp++;break;//Discard anything else
+            case 0:free(t);free(i);return 0;
+            default:ip++;break;//Discard anything else
         }
     }
 }
